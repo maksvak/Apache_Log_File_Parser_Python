02dc8097ec86493f953ef135cd4d3fb6c5e2bc0e
Maksim Vakulenko <aws@makswerks.com>
2018-05-12 09:07:40 -0500

    info update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Apache_Log_File_Parser_Python
-Python Apache Log File Parser I created for a job interview 
+Python Apache Log File Parser I created for a job interview
 
 
 Place weblog csv into same folder as alp.py on desktop - or alter code as it is hard pathed. Then simply navigate to python.exe and run
@@ -8,8 +8,6 @@ python.exe C:\Users\...\Desktop\ALP\alp.py
 
 You will generate two print outs. The first is a list of unique IP's that appear. The script is commented heavily, but as a general explanation:
 
-the function unique basically compares each new string to the stored list. Any new strings get appeneded, any duplicates rejected. 
+the function unique basically compares each new string to the stored list. Any new strings get appeneded, any duplicates rejected.
 
-The dictionary is a key/value store. We have a key that is the host name file, and the value is actually a list, using a similar sort we add the IPs for each key to the list thats stored as it value. Duplicates are ignored. 
-
-This code was written as part of a preinterview screening. 
+The dictionary is a key/value store. We have a key that is the host name file, and the value is actually a list, using a similar sort we add the IPs for each key to the list thats stored as it value. Duplicates are ignored.
--- alp.py
+++ alp.py
@@ -1,35 +1,34 @@
 #Apache Log Parser
 #2/15/2018
 #Created by: Maksim Vakulenko
-#Security Question Assessment
 ##########################################################
-###############################Import Libraries 
-import csv #import csv library 
+###############################Import Libraries
+import csv #import csv library
 import re #import regex
 from collections import defaultdict #imports default dict for better info handling
 
-###############################Step 1: Import the CSV file. 
+###############################Step 1: Import the CSV file.
 #absolute path required but can be made an input for automation
-path = r'C:\Users\maksi\Desktop\ALP\weblog.csv' 
+path = r'C:\Users\maksi\Desktop\ALP\weblog.csv'
 with open(path, 'r') as f:#read in the CSV file
 	reader=csv.reader(f)
 	your_list=list(reader) #puts each line into a string as a part of a list
 
 #############################Step 2: Sort & Separate Information
 
-#Idea here is to use regex in order to break this apart into 
-#the desired pieces. 
+#Idea here is to use regex in order to break this apart into
+#the desired pieces.
 length=len(your_list) #length of list, needed for sort & loops
 i=0 #counter
 ip_list=[]#create a list for IPs
 client_list=[]#create a list of clients
 ip_by_clients = {}
 for i in range(length):
-	xline=your_list[i] #pull line item from csv 
+	xline=your_list[i] #pull line item from csv
 	xline_str=''.join(xline)#convert to string for regex process
 	breakdown=re.findall('\[[^\]]*\]|\([^\)]*\)|\"[^\"]*\"|\S+',xline_str) #split string by spaces
 	ip_temp=breakdown[0]#select origin IP
-	client_temp=breakdown[8]#client name 
+	client_temp=breakdown[8]#client name
 	ip_list.append(ip_temp)#add all IPs to list
 	client_list.append(client_temp)#add all clients to list
 	if client_temp in ip_by_clients:
@@ -37,7 +36,7 @@ for i in range(length):
 		#if the client is already in the dictionary append the ip list
 	else:
 		ip_by_clients[client_temp] = [ip_temp]
-		#create a new pair for the client name 
+		#create a new pair for the client name
 	i+=1#increment counter
 
 #now we have a list of individual IP's' and clients
@@ -46,15 +45,15 @@ def unique(list1): #function for removing duplicates
 
   # intilize a list
     unique_list = []
-     
+
     # traverse for all elements
     for x in list1:
         # check if exists in unique_list or not
         if x not in unique_list:
             unique_list.append(x)
     return (unique_list)
-	
-#############################Step 3: Print Desired Information	
+
+#############################Step 3: Print Desired Information
 #Print Unique IP List
 uniqueip=unique(ip_list)
 for x in uniqueip:
@@ -65,5 +64,3 @@ for client in sorted(ip_by_clients.keys()): #sorts the hosts and post the IP's
 	print(client)
 	for ip_addr in  unique(ip_by_clients[client]):
 		print("    " + ip_addr)
-		
-
